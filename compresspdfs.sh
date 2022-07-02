4cf1f7859ac90b9e76c913fcd42c158746d10483
pabenito <pedrobenitowork@gmail.com>
2022-07-02 18:52:25 +0200

    There are no PDFs message

--- compresspdfs.sh
+++ compresspdfs.sh
@@ -2,12 +2,18 @@
 
 # Sub functions
 
+count_lines(){
+  return `printf "$1" | wc --lines`
+}
+
 get_dir(){
   dir=$1
+  
   if [ -z $dir ]
   then 
     dir="."
   fi
+  
   if [ ! -d $dir ]
   then 
     printf "Error: The specified directory '$dir' does not exist\n" >&2
@@ -19,6 +25,7 @@ get_all_pdfs(){
   local dir=$1
   pdf_paths=""
   printf "Searching PDFs...\n"
+
   for path in `find $dir -print`
   do 
     local ext="${path##*.}"
@@ -27,11 +34,19 @@ get_all_pdfs(){
       pdf_paths+="$path\n"  
     fi
   done
+ 
+  count_lines $pdf_paths
+  local num_of_pdfs=$?
+  if [ $num_of_pdfs -le 0 ]
+  then
+    printf "There are no PDFs in the specified directory '$dir'\n"
+    exit 0
+  fi
 }
 
 compress_pdfs(){
   local pdf_paths=$1
-  local num_of_pdfs=`printf $pdf_paths | wc -l`
+  local num_of_pdfs=$(count_lines $pdf_paths)
   local pdf_paths=`printf $pdf_paths`
 
   local i=1
